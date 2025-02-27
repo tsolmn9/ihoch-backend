e583945b7c6923ab1e6a0dcdb3ec674162ea1d1b
Taffyyy123 <tsolmn9@gmail.com>
2025-02-27 21:17:26 +0800

    bug fixed

--- routes/fromUsRoute.js
+++ routes/fromUsRoute.js
@@ -1,6 +1,12 @@
 const Router = require("express");
 const fromUsRouter = Router();
 
+const {
+  createContactUs,
+  getContact,
+  editContact,
+} = require("../controllers/contactUsCtrl");
+
 fromUsRouter.post("/createFromUsInfo", createContactUs);
 fromUsRouter.get("/getFromUsInfo", getContact);
 fromUsRouter.put("/fromUsEdit", editContact);
