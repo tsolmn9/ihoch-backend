4f5fc26a30b04f4dca1e3546eb03a7540a2d748c
Taffyyy123 <tsolmn9@gmail.com>
2025-03-18 08:08:36 +0800

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://ikh-och.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type, Authorization",
   })
